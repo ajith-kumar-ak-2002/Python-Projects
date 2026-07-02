27c3bc0bcfdb3be284b13e4e0233a8fccbb0f547
Ajith Kumar <ajithkumaraj0206@gmail.com>
2026-07-02 17:07:42 +0530

    Added Rock Paper Scissors Game Project

---
+++ 3_mini_python_project/rock_paper_scissors_game.py
@@ -0,0 +1,21 @@
+import random
+
+choices = ["rock", "paper", "scissors"]
+
+user_choice = input("Enter your choice (rock /paper/scissors): ")
+
+computer_choice = random.choice(choices)
+
+print("You chose, ", user_choice)
+print("computer chose, " , computer_choice)
+
+
+if user_choice == computer_choice:
+    print("It's a tie!")
+elif (user_choice == "rock" and computer_choice == "scissors") or \
+     (user_choice == "paper" and computer_choice == "rock") or \
+     (user_choice == "scissors" and computer_choice == "paper"):
+     print("You Won the game!")
+
+else:
+     print("computer won the Game!")
